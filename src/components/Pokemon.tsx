813aa078dfdd6c34ccccfb01a8ed1194a89e70b4
sabrinacouto <sabrinacoutolima@gmail.com>
2024-04-04 22:10:45 -0300

    criando grids para exibir pokemons

--- src/components/Pokemon.tsx
+++ src/components/Pokemon.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import PokemonItem from "./PokemonItem";
 
 function Pokemon() {
     const [pokemonName, setPokemonName] = useState("");
@@ -47,7 +48,25 @@ function Pokemon() {
   }, []);
 
     return (
-        <div className="h-screen flex items-center justify-center flex-col bg-gray-200">
+      <div>
+           <h1 className="text-center font-bold text-3xl my-10">Pokédex</h1>
+        
+          {!pokemonData && (
+          <div className="flex justify-center">
+            <div>
+              <h1 className="text-xl font-semibold  text-red-500 mb-10">Conheça alguns dos pokemons!</h1>
+              <div className="grid grid-cols-4 gap-4">
+                {pokemonList.map((pokemon, index) => (
+                  <div key={index} className="p-4 bg-white rounded-xl shadow-md">
+                     <PokemonItem key={index} name={pokemon.name} url={pokemon.url} />
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        )}
+      <h1 className="text-center font-bold text-3xl my-10">Qual pokemon você está procurando?</h1>
+        <div className="flex items-center justify-center gap-7">
         <input
           type="text"
           className="px-8 py-4 rounded-lg border-2 border-red-200 my-4"
@@ -55,18 +74,20 @@ function Pokemon() {
           onChange={(e) => setPokemonName(e.target.value)}
         />
         <button
-          className="px-6 py-4 rounded-full bg-red-500 text-white"
+          className="px-3 py-4 rounded-xl bg-red-500 text-white"
           onClick={handleSearch}
         >
           Encontre o seu Pokemon!
         </button>
-      
         {error && <p className="text-red-500">{error}</p>}
+     </div>
+      
+  
       
         {pokemonData && (
-          <div className="flex items-center justify-center flex-col w-80 p-8 rounded-xl mt-8 shadow-md bg-white">
+          <div className="w-80 p-8 rounded-xl mt-8 shadow-md bg-white mx-auto my-auto flex flex-col items-center justify-center">
             <h1 className="text-red-500 text-xl font-bold">{pokemonData.name}</h1>
-            <img src={pokemonData.sprites.front_default} alt={pokemonData.name} className="w-[200px] h-[200px]" />
+            <img src={pokemonData.sprites.other.dream_world.front_default} alt={pokemonData.name} className="w-[200px] h-[200px]" />
             <h2 className="mt-4 text-red-500 text-xl font-bold">Habilidades:</h2>
             <ul className="text-gray-500 font-bold text-lg">
               {pokemonData.abilities.map((ability, index) => (
@@ -75,22 +96,11 @@ function Pokemon() {
             </ul>
           </div>
         )}
+       
 
-{!pokemonData && (
-<div>
-            
-            <div className="w-80 p-8 rounded-xl mt-8 shadow-md bg-white">
-                <h1 className="text-xl font-semibold mb-4 text-red-500">Conheça alguns dos pokemons!</h1>
-                <ul>
-                    {pokemonList.map((pokemon, index) => (
-                        <li key={index} className="text-gray-500 font-bold text-lg">{pokemon.name}</li>
-                       
-                    ))}
-                </ul>
-            </div>
-        </div>
-)}
+
       </div>
+
 
       
       
---
+++ src/components/PokemonItem.tsx
@@ -0,0 +1,32 @@
+// Componente PokemonItem.js
+import React, { useState, useEffect } from "react";
+
+function PokemonItem({ name, url }) {
+  const [imageUrl, setImageUrl] = useState("");
+
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error("Falha ao buscar dados do Pokémon.");
+        }
+        const data = await response.json();
+        setImageUrl(data.sprites.front_default);
+      } catch (error) {
+        console.error('Error fetching Pokemon data:', error);
+      }
+    };
+
+    fetchPokemonData();
+  }, [url]);
+
+  return (
+    <div className="pokemon-item">
+      <h3 className="text-xl font-bold text-red-600">{name}</h3>
+      <img src={imageUrl} alt={name} />
+    </div>
+  );
+}
+
+export default PokemonItem;
